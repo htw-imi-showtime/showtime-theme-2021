b5498086b637938e6443425483e9f45902fd8276
Luis Hankel <kaes3kuch3n@gmail.com>
2020-12-03 23:53:23 +0100

    Added missing files

---
+++ static/js/title-animate.js
@@ -0,0 +1,50 @@
+let title;
+let hovered = false;
+
+window.addEventListener("DOMContentLoaded", () => {
+    title = document.querySelector("nav > a > strong");
+    afterTitle = title.querySelector("::after");
+    title.addEventListener("mouseenter", showFullTitle);
+    title.addEventListener("mouseleave", hideFullTitle);
+});
+
+function showFullTitle() {
+    setTimeout(() => {
+        title.innerText = "IMI⨯SHTI";
+    }, 50);
+    setTimeout(() => {
+        title.innerText = "IMI⨯SHOTIM";
+    }, 100);
+    setTimeout(() => {
+        title.innerText = "IMI⨯SHOWTIME";
+    }, 150);
+    setTimeout(() => {
+        title.innerText = "IMI⨯SHOWTIME_";
+        hovered = true;
+        blink(true);
+    }, 200);
+}
+
+function hideFullTitle() {
+    hovered = false;
+    setTimeout(() => {
+        title.innerText = "IMI⨯SHOTIM";
+    }, 50);
+    setTimeout(() => {
+        title.innerText = "IMI⨯SHTI";
+    }, 100);
+    setTimeout(() => {
+        title.innerText = "IMI⨯ST";
+    }, 150);
+}
+
+function blink(shown) {
+    setTimeout(() => {
+        if (hovered === false) return;
+        if (shown)
+            title.innerText = title.innerText.replace("_", " ");
+        else
+            title.innerText = title.innerText.replace(" ", "_");
+        blink(!shown);
+    }, 400);
+}
